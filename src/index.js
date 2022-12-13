bd9b274cbd86eab6be6d20cf91d64838dc585cc7
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2022-12-14 00:45:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import './css/styles.css';
 
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const url = 'https://restcountries.com/v3.1/all';
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) throw new Error('There was an error');
+
+        return response.json();
+    })
+    .then(body => {
+        const country = body;
+        console.log(country[200].capital);
+        console.log(typeof body );
+        const poland = body.find(({ cca2 }) => cca2 === "PL");
+        console.log(poland);
+    }
+       
+  )
+  .catch(error => console.error(error));
+
+searchBox.addEventListener('input', () => {
+  fetchCountries()
+    .then(body => {
+      console.log(body);
+    })
+    .catch(error => console.log(error));
+});
+
+function fetchCountries() {
+  return fetch('https://restcountries.com/v3.1/name/peru').then(response => {
+    if (!response.ok) throw new Error('There was an error');
+
+    return response.json();
+  });
+}
+
+function renderCountries() {
+
+}
+//     .then(body => {
+//        // console.log(body);
+//         const poland = body.find(({ capital }) => capital === 'Warsaw');
+//         console.log(poland);
+//     })
+//   .catch(error => console.error(error))
+
 const DEBOUNCE_DELAY = 300;
